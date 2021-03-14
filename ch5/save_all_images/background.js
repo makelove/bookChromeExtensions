5a8e5cf6ca939530681e24774b40515b610ef8a6
play4fun <play4fun@foxmail.com>
2021-03-14 18:30:00 +0800

    print url

--- ch5/save_all_images/background.js
+++ ch5/save_all_images/background.js
@@ -1,29 +1,31 @@
-chrome.runtime.onInstalled.addListener(function(){
-  chrome.contextMenus.create({//TODO 不能显示？
-    'id':'saveall',
-    'type':'normal',
-    'title':'保存所有图片',
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.contextMenus.create({
+    'id': 'saveall',
+    'type': 'normal',
+    'title': '保存所有图片',
   });
 });
-// chrome.contextMenus.create({//TODO 也不能显示？
-//   'id':'saveall',
-//   'type':'normal',
-//   'title':'保存所有图片',
-// });
 
-chrome.contextMenus.onClicked.addListener(function(info, tab){
-  if(info.menuItemId == 'saveall'){
-    chrome.tabs.executeScript(tab.id, {file: 'main.js'}, function(results){
-      console.log('executeScript:')
-      if (results && results[0] && results[0].length){
-        results[0].forEach(function(url) {
-          chrome.downloads.download({
-            url: url,
-            conflictAction: 'uniquify',
-            saveAs: false
+
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+  if (info.menuItemId == 'saveall') {
+    chrome.tabs.executeScript(tab.id, { file: 'main.js' },
+      function (results) {
+        console.log('结果多少条:', results[0].length)
+        console.log(results)
+        if (results && results[0] && results[0].length) {
+          results[0].forEach(function (url) {
+            console.log(url)
+            if (url.length > 0) {
+              chrome.downloads.download({
+                url: url,
+                conflictAction: 'uniquify',
+                saveAs: false
+              });
+            }
           });
-        });
+        }
       }
-    });
+    );
   }
 });
