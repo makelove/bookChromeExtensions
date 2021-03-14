5f7f6637fd6b964ff1e189657318f7754af2f82e
play4fun <play4fun@foxmail.com>
2021-03-14 11:14:27 +0800

    美元兑人民币

--- ch3/usd_price/js/background.js
+++ ch3/usd_price/js/background.js
@@ -1,8 +1,10 @@
+//搜索：美元兑人民币
 function httpRequest(url, callback){
     var xhr = new XMLHttpRequest();
     xhr.open("GET", url, true);
     xhr.onreadystatechange = function() {
         if (xhr.readyState == 4) {
+            console.log('xhr.responseText:',xhr.responseText);
             callback(xhr.responseText);
         }
     }
@@ -32,18 +34,18 @@ function updateAmount(amount, exchange){
 }
 
 function gotoYahoo(text, disposition){
-    window.open('http://finance.yahoo.com/q?s=USDCNY=X');
+    window.open('http://finance.yahoo.com/q?s=USDCNY=X');//打开慢
 }
 
-//TODO 有问题
+//失效了
 var url = 'http://query.yahooapis.com/v1/public/yql?q=select%20Rate%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22USDCNY%22)&env=store://datatables.org/alltableswithkeys&format=json';
-var price;
-
-httpRequest(url, function(r){//
-    price = JSON.parse(r);
-    price = price.query.results.rate.Rate;
-    price = Number(price);
-});
+var price=6.51;//汇率
+//取消访问
+// httpRequest(url, function(responseText){//
+//     price = JSON.parse(responseText);
+//     price = price.query.results.rate.Rate;
+//     price = Number(price);
+// });
 
 chrome.omnibox.setDefaultSuggestion({'description':'Find current USD price.'});
 
